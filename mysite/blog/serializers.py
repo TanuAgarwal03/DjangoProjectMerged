1379bf50aa5500456728acd81a2b495e0365d018
Tanu <tanuagarwal.0305@gmail.com>
2024-03-18 22:07:15 +0530

    code modified

--- mysite/blog/serializers.py
+++ mysite/blog/serializers.py
@@ -4,23 +4,26 @@ from rest_framework.authtoken.models import Token
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 
-
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = super().create(validated_data)
         user.set_password(password)
         user.save()
-        token, created = Token.objects.get_or_create(user=user)
         return user
 
     def get_token(self, obj):
         token, created = Token.objects.get_or_create(user=obj)
-        return token.key 
+        return token.key
 
 
 class PostSerializer(serializers.ModelSerializer):
